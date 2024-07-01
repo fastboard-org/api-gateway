2ea2120ddfd58cd85b0bf93ed3460c24c861c52e
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-07-01 12:55:47 -0300

    feat: Added schemas for all CRUD operations of the dashboards service: Dashboard, Folder, Connection and Query

---
+++ src/schemas/connections.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class CreateConnection(BaseModel):
+    name: str
+    type: str
+    credentials: dict
+    variables: dict
+
+
+class UpdateConnection(BaseModel):
+    name: Optional[str] = None
+    credentials: Optional[dict] = None
+    variables: Optional[dict] = None
+
+
+class GetConnections(BaseModel):
+    user_id: Optional[str] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
