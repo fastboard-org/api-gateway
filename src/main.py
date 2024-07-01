de818f4dcfcc0404d42f593e24b5020835960d58
TGRodriguez <tgrodriguez@fi.uba.ar>
2024-07-01 12:58:08 -0300

    feat: Added exceptions handlers and routers to the FastAPI app, and added a lifespan function to initialize the firebase app

--- src/main.py
+++ src/main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI
 import uvicorn
-from settings import settings
+from configs.settings import settings
 from routers.users_router import UsersRouter
+from routers.dashboards.dashboards import DashboardsRouter
+from routers.dashboards.folders import FoldersRouter
+from routers.dashboards.connections import ConnectionsRouter
+from routers.dashboards.queries import QueriesRouter
+from auth.firebase import initialize_firebase
+from contextlib import asynccontextmanager
+from errors import handle_custom_exception
+from errors import handle_validation_error
+from fastapi.exceptions import RequestValidationError
+from errors import CustomException
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    print("Starting firebase admin")
+    initialize_firebase()
+    yield
+    print("Closing app")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(UsersRouter)
+app.include_router(DashboardsRouter)
+app.include_router(FoldersRouter)
+app.include_router(ConnectionsRouter)
+app.include_router(QueriesRouter)
+app.add_exception_handler(RequestValidationError, handle_validation_error)
+app.add_exception_handler(CustomException, handle_custom_exception)
 
 
 if __name__ == "__main__":
