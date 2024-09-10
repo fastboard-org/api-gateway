589c8c3be5e0e46e01a21991cefadcfda2699f32
TGRodriguez <66755369+TGRodriguez@users.noreply.github.com>
2024-09-10 20:33:17 -0300

    feat: Changed PreviewQuery to use either ApiMetadata or MongoMetadata (#17)

--- src/schemas/adapter.py
+++ src/schemas/adapter.py
@@ -1,13 +1,25 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class ExecuteQuery(BaseModel):
     parameters: dict
 
 
-class PreviewQuery(BaseModel):
-    parameters: dict
+class ApiMetadata(BaseModel):
     method: str
     path: str
     headers: dict
     body: dict
+
+
+class MongoMetadata(BaseModel):
+    method: str
+    collection: str
+    filter_body: dict
+    update_body: dict
+
+
+class PreviewQuery(BaseModel):
+    parameters: dict
+    connection_metadata: Union[ApiMetadata, MongoMetadata]
